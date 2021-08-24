f0812b8efcb3f4c4237b5c5119e798e7f0fed477
patric noriega <patricnoriega@gmail.com>
2021-08-24 13:22:31 -0500

    exercise 1-7 intro-to-testing complete

--- code.js
+++ code.js
@@ -8,9 +8,15 @@ function helloWorld() {
 //     return "Hello, World!";
 // }
 
+// function sayHello(input) {
+//     if (input === "Alex") {
+//         return "Hello, Alex!";
+//     }if(input === "Pat") {
+//         return "Hello, Pat!";
+//     }else
+//     return "Hello, Jane!";
+// }
+
 function sayHello(input) {
-    if (input === "Alex") {
-        return "Hello, Alex!"
-    }else
-    return "Hello, Jane!";
+    return "Hello, " + input + "!";
 }
