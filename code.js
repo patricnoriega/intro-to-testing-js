f8fc1f117a54f5b9d247be3e99d431cc51b41ec1
patric noriega <patricnoriega@gmail.com>
2021-08-24 11:58:08 -0500

    exercise 5 added

--- code.js
+++ code.js
@@ -8,6 +8,9 @@ function helloWorld() {
 //     return "Hello, World!";
 // }
 
-function sayHello() {
-    return "Hello,";
+function sayHello(input) {
+    if (input === "Alex") {
+        return "Hello, Alex!"
+    }else
+    return "Hello, Jane!";
 }
--- tests.js
+++ tests.js
@@ -19,4 +19,13 @@ describe("sayHello", function () {
     it("should be a defined function", function () {
         expect(typeof sayHello).toBe("function");
     });
+    it("should return a string when called", function () {
+        (expect(typeof sayHello()).toBe("string"));
+    });
+    it("should return the string 'Hello, Jane!' when executed", function () {
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it("should return the string 'Hello, Alex!' when executed", function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!")
+    });
 });
