893d1441f391b88f30b730b26affdd2cb88f996a
patric noriega <patricnoriega@gmail.com>
2021-08-24 11:26:06 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-// helloWorld function 
+// // helloWorld function
+
 function helloWorld() {
     return "Hello, World!";
+}
+
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+function sayHello() {
+    return "Hello,";
 }
--- tests.js
+++ tests.js
@@ -12,4 +12,11 @@ describe('helloWorld', function() {
     it("should never return 'undefined' when called", function() {
       expect(helloWorld()).not.toBe(undefined);
     });
+
+});
+
+describe("sayHello", function () {
+    it("should be a defined function", function () {
+        expect(typeof sayHello).toBe("function");
+    });
 });
